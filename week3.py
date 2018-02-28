c3d61e10a3d13c6110cee0bbcf9770616040da64
nanlin0115 <36024595+nanlin0115@users.noreply.github.com>
2018-02-27 20:24:57 -0500

    Update week3.py

--- week3.py
+++ week3.py
@@ -1,83 +1,64 @@
-from flask import Flask, request, jsonify, session, render_template
+from flask import Flask, request, jsonify, render_template
+import os
+import pymysql
 import pymysql.cursors
 
-app = Flask(__name__) 
-conn = pymysql.connect(host='localhost', database='TODO', user='cs1122', password='cs1122', charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
-#connect to a local host database
+app = Flask(__name__)
+conn = pymysql.connect(host='localhost', database='TODO', user='cs1122', password='cs122')
+cur = conn.cursor()
 
 @app.route('/')
-def showWebpage():
-	with conn.cursor() as cursor:
-		databases = cursor.execute("SHOW DATABASES").fetchall()
-		print("Databases: "+databases)
-		#Go inside within a database here		cursor.execute("USE insert_database_name")	
-		tables = cursor.execute("SHOW TABLES").fetchall()
-		print("Tables: "+tables)
-		#get columns of a table here 				columns = cursor.execute("DESCRIBE insert_table_name")
-		
-
-		for table in data:
-				if table['Tables_in_db'] == 'week3'
-
-		for week3 in data:
-				response[week3['id']] = week3['items']
-
-		json_data = jsonify(dictionary)
-				return json_data
-
-		return render_template('week3.html')
+def index():
+    # session['data'] = []
+    # session.modified = True
+    return render_template('week3.html')
 
 @app.route('/todo/create', methods=['POST'])
-def create():
-	if request.method == 'POST':
-		task = request.json
-		with conn.cursor() as cursor:
-			cursor.execute("INSERT INTO week3(item) VALUES(task)")
-			return 'Success'
-	else:
-		return 'failed'
+def add():
+    if request.method != 'POST':
+        return "error: wrong request type"
+    
+    task = request.json# request what ? 
+    cur.execute("INSERT INTO todos(item) VALUES(%s)",(task))
+    conn.commit()
+    cur.execute("SELECT * FROM todos")
+    items = cur.fetchall()
+    print(items[-1])
+    return jsonify({"result":"success", "task":items[-1]})
 
 @app.route('/todo/read', methods=['GET'])
-def read():
-	dictionary = {}
-	if request.method == 'GET':
-		with conn.cursor() as cursor:
-			cursor.execute("SELECT item FROM week3") #selects all the stuff under items
-			result = cursor.fetchall()
-			for stuff in result:
-				response[stuff['id']] = stuff['item']
-			json_result = jsonify(dictionary)
-			return json_result
-	else:
-		return 'failed'
+def fetch():
+    cur.execute("SELECT * FROM todos")
+    items = cur.fetchall()     
+    return jsonify({"result":"success", "task": items})
 
 @app.route('/todo/update', methods=['PUT'])
 def update():
-	index = request.get_json()['index']
-	task = request.get_json()['task']
-	with conn.cursor() as cursor:
-		cursor.execute("UPDATE week3 SET item =%s WHERE week3.id=%s", (str(task),index))
-		#should we index += 1 because the id goes from 1 to 4, while index goes from 0 to .....
+    if request.method != 'PUT':
+        return "error: wrong request type"
+    if len(request.json) != 2:
+        return 'error: 2 parameters not sent'
+    index = request.get_json()['index']
+    task = request.get_json()['task']
+    cur.execute("UPDATE todos SET item = %s WHERE id = %s",(task, index))
+    conn.commit()
+    return jsonify("success")
 
-
-#Not sure how to implement the delete one because if I delete one id, the numbers on the rest of the ids stay the same, while in the site the list would move up
 @app.route('/todo/delete', methods=['DELETE'])
 def delete():
-		if request.method != 'DELETE':
-				return "error: wrong request type"
-		if len(session['data']) < int(deleteIndex) or len(session['data']) < 0:
-			return "error: index out of range"
+    if request.method != 'DELETE':
+        return "error: wrong request type"
+    deleteIndex = request.json
+    cur.execute("DELETE FROM todos WHERE id = %s",(deleteIndex))
+    conn.commit()
+    return jsonify("success")
 
-		with conn.cursor() as cursor:
-				deleteIndex = request.json
-				delete = "DELETE FROM week3 WHERE id=int(deleteIndex)"
-				cursor.execute(delete)
 
+
+if __name__ == '__main__':
+    app.secret_key = os.urandom(24)
+    app.run(debug=True)
 		# del session['data'][int(deleteIndex)]
 		# session.modified = True
 		# print(session['data']) #print the session to make sure it was deleted
 		# return jsonify("success")
-
-
-if __name__ == '__main__':
-		app.run(debug=True)
